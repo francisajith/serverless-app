3376fe4ba2c88d878099bd0e4584bcc44761d545
francisajith <francisajith4u@gmail.com>
2020-04-08 18:35:36 +0100

    Updates

---
+++ precache-manifest.5600f99d24c555f18b4ea45eccd29ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8d809af564c5abe7917d2f224fa86a4",
+    "url": "/serverless-app/index.html"
+  },
+  {
+    "revision": "0f12020b00f30899938b",
+    "url": "/serverless-app/static/css/main.70b90cf6.chunk.css"
+  },
+  {
+    "revision": "b13b55708c268e40ba83",
+    "url": "/serverless-app/static/js/2.508ccb7e.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/serverless-app/static/js/2.508ccb7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f12020b00f30899938b",
+    "url": "/serverless-app/static/js/main.de2cf9f3.chunk.js"
+  },
+  {
+    "revision": "88617db91cc188d640a7",
+    "url": "/serverless-app/static/js/runtime-main.b73e587d.js"
+  }
+]);
